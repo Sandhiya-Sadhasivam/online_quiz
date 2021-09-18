0bc69225625d4badbfc31ebf80f6ab87a3d7e79b
Rajeshwaran <54212165+Rajeshwaran2001@users.noreply.github.com>
2021-09-18 19:03:13 +0530

    app deleted and model created

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+# Register models which should be modified my admin
+admin.site.register(MyUser)
+admin.site.register(Answer)
+admin.site.register(Question, AdminQuestion)
+admin.site.register(UserAnswer)
