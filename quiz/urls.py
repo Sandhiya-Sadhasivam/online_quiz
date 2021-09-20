1601c42f8b95e63d231ae81ec453e764820e7a63
Rajeshwaran <54212165+Rajeshwaran2001@users.noreply.github.com>
2021-09-20 20:02:32 +0530

    minor fix

--- quiz/urls.py
+++ quiz/urls.py
@@ -1,5 +1,4 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
 from quiz import views
 
@@ -7,5 +6,7 @@ app_name = "quiz"
 
 urlpatterns = [
     path('list', views.student_exam_view, name='quiz-list'),
-    path('take-quiz/<int:pk>', views.take_exam_view,name='take-quiz'),
+    path('take-quiz/<int:pk>', views.take_exam_view, name='take-quiz'),
+    path('start-quiz/<int:pk>', views.start_exam_view, name='start-quiz'),
+    path('calculate-marks', views.calculate_marks_view, name='calculate-marks'),
 ]
--- quiz/views.py
+++ quiz/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from quiz import models as QMODEL
 
@@ -21,3 +22,38 @@ def take_exam_view(request, pk):
 
     return render(request, 'quiz/take-quiz.html',
                   {'course': course, 'total_questions': total_questions, 'total_marks': total_marks})
+
+
+@login_required()
+def start_exam_view(request, pk):
+    course = QMODEL.Course.objects.get(id=pk)
+    questions = QMODEL.Question.objects.all().filter(course=course)
+    if request.method == 'POST':
+        pass
+    response = render(request, 'quiz/start-quiz.html', {'course': course, 'questions': questions})
+    response.set_cookie('course_id', course.id)
+    return response
+
+
+@login_required()
+def calculate_marks_view(request):
+    if request.COOKIES.get('course_id') is not None:
+        course_id = request.COOKIES.get('course_id')
+        course = QMODEL.Course.objects.get(id=course_id)
+
+        total_marks = 0
+        questions = QMODEL.Question.objects.all().filter(course=course)
+        for i in range(len(questions)):
+
+            selected_ans = request.COOKIES.get(str(i + 1))
+            actual_answer = questions[i].answer
+            if selected_ans == actual_answer:
+                total_marks = total_marks + questions[i].marks
+        student = models.User.objects.get(user_id=request.user.id)
+        result = QMODEL.Result()
+        result.marks = total_marks
+        result.exam = course
+        result.student = student
+        result.save()
+
+        return HttpResponseRedirect('view-result')
