6773b839879903b2bb778a065d84914a4ee32204
Rajeshwaran <54212165+Rajeshwaran2001@users.noreply.github.com>
2021-09-26 21:29:24 +0530

    appreciation certificate is created

--- certificate/views.py
+++ certificate/views.py
@@ -52,7 +52,7 @@ def generate(request):
             draw.text((1035, 900), org[i], font=fontTwo, fill="black")
             # draw.text((935, 80), mail[i], font=fontTwo , fill ="black")
 
-            image.save("static/certificates/%s.jpg" % uniqueId)
+            image.save("static/certificates/winners/%s.jpg" % uniqueId)
 
             # Email
 
@@ -61,7 +61,7 @@ def generate(request):
                 'Hi, this is your certificate for completing the course, you can verify this certificate using this unique ID: certify.com/verify/%s  ' % uniqueId,
                 'rajeshwaran1718@gmail.com',
                 [mail[i]])
-            email.attach_file('static/certificates/%s.jpg' % uniqueId)
+            email.attach_file('static/certificates/winners/%s.jpg' % uniqueId)
 
             email.send()
             # Email
@@ -76,7 +76,7 @@ def generate(request):
 def verify(request, slug):
     # found = False
     context = {'info': 'NotFound', 'flag': False}
-    for f in os.listdir('static/certificates'):
+    for f in os.listdir('static/certificates/winners'):
         fn, fext = os.path.splitext(f)
         print(fn)
         if fn == slug:
@@ -87,7 +87,79 @@ def verify(request, slug):
 
 
 
+def appreciation_generate(request):
+    if request.method == "POST":
+        fileUpload = request.FILES['file']
+        f = FileSystemStorage()
+        f.save(fileUpload.name, fileUpload)
+        fileName = fileUpload.name
 
+        df = pd.read_excel('uploads/%s' % fileName, sheet_name='Sheet1')
+        n = df.columns[0]
+        o = df.columns[1]
+        c = df.columns[2]
+        m = df.columns[3]
+
+        fullName = df[n]
+        org = df[o]
+        cer = df[c]
+        mail = df[m]
+        for i in range(len(df)):
+            uniqueId = shortuuid.uuid()
+            # Email
+
+            # data base save
+            detail = details(name=fullName[i], organization=org[i], certification=cer[i], mail=mail[i], uid=uniqueId)
+            detail.save()
+            # Email
+
+            # certificate generation
+
+            image = Image.open('template/template1.jpg')
+            font = ImageFont.truetype("arial.ttf", 70)
+            fontTwo = ImageFont.truetype("arial.ttf", 50)
+
+            draw = ImageDraw.Draw(image)
+
+            # use a bitmap font
+            draw.text((500, 680), fullName[i], font=font, fill="black")
+            # use a truetype font
+            draw.text((1266, 850), cer[i], font=fontTwo, fill="black")
+            draw.text((1035, 900), org[i], font=fontTwo, fill="black")
+            # draw.text((935, 80), mail[i], font=fontTwo , fill ="black")
+
+            image.save("static/certificates/participant/%s.jpg" % uniqueId)
+
+            # Email
+
+            email = EmailMessage(
+                'Here is you certificate ',
+                'Hi, this is your appreciation certificate, you can verify this certificate using this unique ID: certify.com/verify/%s  ' % uniqueId,
+                'rajeshwaran1718@gmail.com',
+                [mail[i]])
+            email.attach_file('static/certificates/participant/%s.jpg' % uniqueId)
+
+            email.send()
+            # Email
+
+        os.remove('uploads/%s' % fileName)
+        return render(request, 'certificate/genrate.html')
+    else:
+
+        return render(request, 'certificate/genrate.html')
+
+
+def appreciation_verify(request, slug):
+    # found = False
+    context = {'info': 'NotFound', 'flag': False}
+    for f in os.listdir('static/certificates/participant'):
+        fn, fext = os.path.splitext(f)
+        print(fn)
+        if fn == slug:
+            context = {'info': slug, 'flag': True}
+            found = True
+            print("found the certificate")
+    return render(request, 'certificate/verify.html', context)
 
 
 
