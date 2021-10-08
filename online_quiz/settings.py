bc5f65d2fc5a61a7c9b43086d52434b67efd3831
Rajeshwaran <54212165+Rajeshwaran2001@users.noreply.github.com>
2021-10-08 18:13:24 +0530

    Update settings.py

--- online_quiz/settings.py
+++ online_quiz/settings.py
@@ -129,8 +129,8 @@ MEDIA_URL = '/uploads/'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = '587'
-EMAIL_HOST_USER = 'rajeshwaran1718@gmail.com'
-EMAIL_HOST_PASSWORD = 'R@je$h1718'
+EMAIL_HOST_USER = 'youremail@gmail.com'
+EMAIL_HOST_PASSWORD = 'your email password'
 EMAIL_USE_TLS = True
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
