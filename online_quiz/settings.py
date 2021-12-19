59e50a2589d7384590d020135217f8c01b8033e0
Rajeshwaran <54212165+Rajeshwaran2001@users.noreply.github.com>
2021-12-19 21:28:13 +0530

    update for release

--- online_quiz/settings.py
+++ online_quiz/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-gxzy8=$@!4zzfpv!jnzw(5j#t=rbsqtq*7%7&kx9ddyh8-f2(t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['onlinee-quiz.herokuapp.com']
 
 # Application definition
 
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -77,9 +78,20 @@ WSGI_APPLICATION = 'online_quiz.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+'default': {
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'd82hj5je3h62h2',
+
+        'USER': 'seioegihiomsne',
+
+        'PASSWORD': 'e39d41b14ee77cf7ccb4f2f2c064650243e8170d49405509b8f305eacb1d591b',
+
+        'HOST': 'ec2-44-199-86-61.compute-1.amazonaws.com',
+
+        'PORT': '5432',
+
     }
 }
 
@@ -116,7 +128,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+-STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static/')
