81ff3ff61c7c4d2821b743964b71a2244567af1c
Rajeshwaran <54212165+Rajeshwaran2001@users.noreply.github.com>
2021-12-19 23:18:58 +0530

    v20

--- online_quiz/urls.py
+++ online_quiz/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from . import views
@@ -25,4 +27,4 @@ urlpatterns = [
     path('quiz/', include('quiz.urls')),
     path('accounts/', include('accounts.urls')),
     path('certificate/',include('certificate.urls')),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
